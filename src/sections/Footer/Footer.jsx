07d85debdce9ad7714ecca080cd2adde8ed68e83
prathamesh <prathamesh@shipdelight.com>
2024-06-20 00:25:32 +0530

    initial commit

---
+++ src/sections/Footer/Footer.jsx
@@ -0,0 +1,13 @@
+import styles from './FooterStyles.module.css';
+
+function Footer() {
+  return (
+    <section id="footer" className={styles.container}>
+      <p>
+        <b>Created With</b> ❤️ <b>By Prathamesh</b>
+      </p>
+    </section>
+  );
+}
+
+export default Footer;
